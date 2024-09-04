a8049b26f715eae58de7fdff66cf99fe90e081c0
karen07 <karen0734@gmail.com>
2024-09-05 00:51:59 +0300

    New version

--- src/url-block-test.c
+++ src/url-block-test.c
@@ -52,27 +52,20 @@ int main(int argc, char *argv[])
     }
 
     char line_buf[PACKET_MAX_SIZE];
-    char curl_slist_append_data[PACKET_MAX_SIZE * 3];
-    char curl_easy_setopt_data[PACKET_MAX_SIZE * 3];
-
-    int32_t count = 0;
+    char curl_easy_setopt_data[PACKET_MAX_SIZE + 20];
 
     while (fscanf(fp, "%s", line_buf) != EOF) {
-        count++;
-
         CURL *curl;
         CURLcode res = CURLE_OK;
 
-        sprintf(curl_slist_append_data, "new.%s:443:speedtest.selectel.ru:443", line_buf);
-
-        struct curl_slist *host = curl_slist_append(NULL, curl_slist_append_data);
+        struct curl_slist *host = curl_slist_append(NULL, "::speedtest.selectel.ru");
 
         curl = curl_easy_init();
         if (curl) {
-            sprintf(curl_easy_setopt_data, "https://new.%s/100MB", line_buf);
+            sprintf(curl_easy_setopt_data, "https://%s/100MB", line_buf);
 
             curl_easy_setopt(curl, CURLOPT_CONNECT_TO, host);
-            curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT_MS, 200);
+            curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT_MS, 100);
             curl_easy_setopt(curl, CURLOPT_CONNECT_ONLY, 1L);
             curl_easy_setopt(curl, CURLOPT_URL, curl_easy_setopt_data);
             curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
@@ -84,7 +77,8 @@ int main(int argc, char *argv[])
         curl_slist_free_all(host);
 
         if (res == CURLE_OPERATION_TIMEDOUT) {
-            printf("%d new.%s\n", count, line_buf);
+            printf("%s\n", line_buf);
+            fflush(stdout);
         }
     }
 
